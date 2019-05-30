5c32299a76dadbbdc244747be715016448070455
Brayan Angarita <brayanangarita11@gmail.com>
2019-05-30 12:22:21 -0500

    Punto fijo added

---
+++ Puntofijo/Puntofijo/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Puntofijo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxEpsilon = new System.Windows.Forms.TextBox();
+            this.hallar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(107)))));
+            this.textBoxX.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxX.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxX.Location = new System.Drawing.Point(141, 36);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxEpsilon
+            // 
+            this.textBoxEpsilon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(107)))));
+            this.textBoxEpsilon.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxEpsilon.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEpsilon.Location = new System.Drawing.Point(141, 96);
+            this.textBoxEpsilon.Name = "textBoxEpsilon";
+            this.textBoxEpsilon.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEpsilon.TabIndex = 1;
+            // 
+            // hallar
+            // 
+            this.hallar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(21)))));
+            this.hallar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hallar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hallar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(107)))));
+            this.hallar.Location = new System.Drawing.Point(27, 188);
+            this.hallar.Name = "hallar";
+            this.hallar.Size = new System.Drawing.Size(226, 30);
+            this.hallar.TabIndex = 2;
+            this.hallar.Text = "Hallar";
+            this.hallar.UseVisualStyleBackColor = false;
+            this.hallar.Click += new System.EventHandler(this.hallar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Epsilon";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.textBoxX);
+            this.panel1.Controls.Add(this.hallar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBoxEpsilon);
+            this.panel1.Location = new System.Drawing.Point(115, 191);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(271, 237);
+            this.panel1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "MÉTODO PUNTO FIJO";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tang^-1 (x) +x -1 = 0";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(21)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(107)))));
+            this.button2.Location = new System.Drawing.Point(164, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ver Grafica";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(107)))));
+            this.button4.Location = new System.Drawing.Point(460, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(43, 29);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "X";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(21)))));
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Location = new System.Drawing.Point(-3, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(506, 29);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(21)))));
+            this.panel3.Location = new System.Drawing.Point(141, 61);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 2);
+            this.panel3.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(21)))));
+            this.panel4.Location = new System.Drawing.Point(141, 121);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(100, 2);
+            this.panel4.TabIndex = 13;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(40)))), ((int)(((byte)(21)))));
+            this.panel5.Location = new System.Drawing.Point(-3, 459);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(506, 11);
+            this.panel5.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(203)))), ((int)(((byte)(107)))));
+            this.ClientSize = new System.Drawing.Size(500, 465);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxEpsilon;
+        private System.Windows.Forms.Button hallar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ Puntofijo/Puntofijo/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puntofijo
+{
+    public partial class Form1 : Form
+    {
+        // tang^-1(x)+x-1=0
+        public static double g2(double x)
+        {
+            return (1 - Math.Atan(x));
+            //x = 1 - tang^-1(x)
+
+
+        }
+
+        public static double g2P(double x)
+        {
+            return (-(1 / ( 1 + 2*x)));
+            // - (1/1+2x)
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hallar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxX.Text))
+            {
+                MessageBox.Show("El valor ingresado no fue el que solicitamos");
+                foreach (Control ctrl in this.Controls)
+                {
+                    if (ctrl is TextBox)
+                    {
+                        ctrl.Text = "";
+                    }
+                }
+
+            }
+
+            else
+            {
+
+
+                double x0 = Convert.ToDouble(value: textBoxX.Text);
+                double valor = x0;
+                double x = x0;
+                double errorPedido = Convert.ToDouble(value: textBoxEpsilon.Text);
+
+                double errorObtenido = 1;
+
+                DateTime tiempo1 = DateTime.Now;
+
+                int contador = 1;
+
+                while (errorObtenido > errorPedido)
+                {
+
+                    if (Math.Abs(g2P(x0)) < 1)
+                    {
+                        double x1 = g2(x0);
+                        errorObtenido = Math.Abs(x1 - x0);
+                        x0 = x1;
+                        contador++;
+                    }
+
+                }
+
+                DateTime tiempo2 = DateTime.Now;
+                TimeSpan total = new TimeSpan(tiempo2.Millisecond + tiempo1.Millisecond);
+
+                MessageBox.Show("La solución aproximada es: " + x0 + "\n\n El número de interacciones fue de " + contador + "\n\n El tiempo de ejecución fue de " + total.ToString());
+                foreach (Control ctrl in this.Controls)
+                {
+                    if (ctrl is TextBox)
+                    {
+                        ctrl.Text = "";
+                    }
+                }
+
+
+
+
+
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Desea salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que deseas salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
